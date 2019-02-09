24ef6b1d3cc7f36a8c2bafd2a2d57f578cf75cf6
hurarara <oshan1112@gmail.com>
2019-02-09 16:20:13 +0900

    Updates

---
+++ precache-manifest.c10a0547be9eba93149023b5a34501f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f3a49634996d7958834",
+    "url": "/hurarara/react-movie-app/static/js/runtime~main.3f3a4963.js"
+  },
+  {
+    "revision": "7048f8f868f3844c950c",
+    "url": "/hurarara/react-movie-app/static/js/main.7048f8f8.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/hurarara/react-movie-app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "7048f8f868f3844c950c",
+    "url": "/hurarara/react-movie-app/static/css/main.48b17f6a.chunk.css"
+  },
+  {
+    "revision": "faa98e9a8de3b411866568133baa8284",
+    "url": "/hurarara/react-movie-app/index.html"
+  }
+];
